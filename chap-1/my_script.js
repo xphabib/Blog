1fe3a8973ccb031a321833f23b47ea0663cd5b14
Your Name <you@example.com>
2018-12-31 11:21:13 +0600

    First Commit

---
+++ chap-1/my_script.js
@@ -0,0 +1,7 @@
+function my_func2() {
+    document.getElementById('habib').innerHTML="rubel";
+}
+
+function my_func(){
+    document.getElementById('my_bangladesh').innerHTML="I love bangladesh";
+}
---
+++ chap-1/my_script2.js
@@ -0,0 +1,3 @@
+function my_func3() {
+    document.getElementById('cocacola').innerHTML="<ul><li>list 1</li><li>list 2</li></ul>";
+}
